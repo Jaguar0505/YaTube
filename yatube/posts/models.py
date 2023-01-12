e5e04a014a1d1f4141dc8681795a18588d26e539
Стас Басов <stas.basov@whatever.com>
2023-01-12 15:02:17 +0700

    Review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -18,10 +18,14 @@ class Post(models.Model):
         blank=True,
         null=True)
 
+    class Meta:
+        ordering = ['-pub_date']
+
 
 class Group(models.Model):
-    def __str__(self):
-        return self.title
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+
+    def __str__(self):
+        return self.title
