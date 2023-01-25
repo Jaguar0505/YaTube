aa4410fac249734eb65802b16416dc37fc57b9c8
Стас Басов <stas.basov@whatever.com>
2023-01-25 15:54:05 +0700

    Password change

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path(
+        'signup/',
+        views.SignUp.as_view(template_name='users/signup.html'),
+        name='signup'),
     path(
         'logout/',
         LogoutView.as_view(template_name='users/logged_out.html'),
@@ -17,9 +20,16 @@ urlpatterns = [
         name='login'
     ),
     path(
-        'password_reset/',
-        PasswordResetView.as_view(
-            template_name='users/password_reset_form.html'),
-        name='password_reset_form'
+        'password_change/',
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'),
+        name='password_change_form'
+    ),
+    path(
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+        ),
+        name='password_change_done'
     ),
 ]
