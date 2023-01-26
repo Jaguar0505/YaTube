2a85225a7b2fb991bc6517a83514537ce4800cd4
Стас Басов <stas.basov@whatever.com>
2023-01-26 17:52:16 +0700

    Password reset

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetView, PasswordResetDoneView
 from django.urls import path
 from . import views
 
@@ -32,4 +32,32 @@ urlpatterns = [
         ),
         name='password_change_done'
     ),
+    path(
+        'password_reset/',
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        ),
+        name='password_reset_form'
+    ),
+    path(
+        'password_reset/done/',
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    )
 ]
